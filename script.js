b4434858f663b34da57a10d532f34b053a1fe0c7
Aung Pyay Thar <132377611+aungpyaythar1998@users.noreply.github.com>
2023-09-24 23:51:16 +0630

    Add options for page number and reading status

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 const myLibrary = [
-    new Book("Lord of the Mysteries", "爱潜水的乌贼"),
-    new Book("Ryn of Avonside", "QuietValerie"),
-    new Book("Flowers in the Dungeon", "JAW (formerly JAK)"),
+    new Book("Lord of the Mysteries", "爱潜水的乌贼", 1000, 1),
+    new Book("Ryn of Avonside", "QuietValerie", 800, 2),
+    new Book("Flowers in the Dungeon", "JAW (formerly JAK)", 120, 3),
 ];
 
-function Book(title, author) {
+function Book(title, author, pages, readingStatusIdx) {
+    let readingStatuses = [
+        "To Read",
+        "Reading",
+        "Finished",
+    ];
     this.title = title;
     this.author = author;
+    this.pages = pages;
+    this.readingStatus = readingStatuses[readingStatusIdx - 1];
 }
 
 
-function ifInvalidInput(str) {
-    let WhitespaceStr = (str.length > 0 && str.trim() === "");
-    let EmptyStr = (str === "");
-    
-    return (WhitespaceStr || EmptyStr);
+function ifInvalidInput(...params) {
+
+    return params.some((eachParam) => {
+        let WhitespaceStr = (eachParam.length > 0 && eachParam.trim() === "");
+        let EmptyStr = (eachParam === "");
+        
+        return (WhitespaceStr || EmptyStr);
+    });
 }
 
-function addBookToLibrary(bookTitle, bookAuthor) {
+function addBookToLibrary(bookTitle, bookAuthor, pages, readingStatus) {
     // ask the book title and author (and maybe book-related questions in the future)
     // if the user presses cancel at any point, stop the prompting
     // if the user completes all question, put an alert that notifies a book has been added
     // if the user types in nothing and hits ok, ask them again.
 
-    if(ifInvalidInput(bookTitle) || ifInvalidInput(bookAuthor)) {
+    if(ifInvalidInput(bookTitle, bookAuthor, pages, readingStatus)) {
         return;
     }
 
-    let bookToAdd = new Book(bookTitle, bookAuthor);
+    let bookToAdd = new Book(bookTitle, bookAuthor, pages, readingStatus);
     myLibrary.push(bookToAdd);
 }
 
@@ -57,11 +67,13 @@ submitBookBtn.addEventListener("click", (e) => {
     let inputFields = document.getElementsByClassName("book-data-input");
     let bookTitle = inputFields[0].value;
     let bookAuthor = inputFields[1].value;
+    let pages = document.getElementById("page-number-input").value;
+    let readingStatus = document.getElementById("reading-status").value;
 
     inputFields[0].value = "";
     inputFields[1].value = "";
 
-    addBookToLibrary(bookTitle, bookAuthor);
+    addBookToLibrary(bookTitle, bookAuthor, pages, readingStatus);
     addBookDialog.close();
 });
 
